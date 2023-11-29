257749ed8d39cb58ef799f4f797ae27e1572016e
Aditya Bhatnagar <bhatanagar88.aditya@gmail.com>
2023-11-29 17:34:21 +0530

    minute changes

--- src/main/java/com/app/chatapp/ChatAppApplication.java
+++ src/main/java/com/app/chatapp/ChatAppApplication.java
@@ -1,17 +1,11 @@
 package com.app.chatapp;
 
-import com.app.chatapp.model.ChatMessage;
-import com.app.chatapp.service.ChatMessageService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ChatAppApplication {
 
-    @Autowired
-    private ChatMessageService chatMessageService;
     public static void main(String[] args) {
         SpringApplication.run(ChatAppApplication.class, args);
     }
